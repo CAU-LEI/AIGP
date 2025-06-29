e09e089f6eb1541dd9041ab3bb471845426e92af
CAU-LEI <154577903+CAU-LEI@users.noreply.github.com>
2025-06-30 05:06:40 +0800

    Update utils.py

--- aigp/utils.py
+++ aigp/utils.py
@@ -1,14 +1,13 @@
-# aigp/utils.py
 import time
 import contextlib
 
 @contextlib.contextmanager
-def Timer(name="任务"):
+def Timer(name="Task"):
     """
-    上下文管理器，用于记录任务运行时间
+    Context manager for measuring and printing task execution time
     """
     start = time.time()
-    print("开始 {}...".format(name))
+    print("Starting {}...".format(name))
     yield
     elapsed = time.time() - start
-    print("{} 完成，耗时：{:.2f}秒".format(name, elapsed))
+    print("{} completed in {:.2f} seconds".format(name, elapsed))
